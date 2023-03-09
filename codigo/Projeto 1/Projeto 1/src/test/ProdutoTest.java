f4c443815605eb5cc6fbfb4518f9bcd56a3e85ce
luanaf4 <103442333+luanaf4@users.noreply.github.com>
2023-03-09 16:58:15 -0300

    Criação do teste da Margem de Lucro do produto #4

--- codigo/Projeto 1/Projeto 1/src/test/ProdutoTest.java
+++ codigo/Projeto 1/Projeto 1/src/test/ProdutoTest.java
@@ -24,6 +24,19 @@ class ProdutoTest {
 	}
 	
 	@Test
+    	void testMargemLucro() {
+        Produto p1 = new Produto();
+        p1.setPrecoCusto(20);
+        p1.setMargemLucro(0.6);
+        assertEquals(0.6, p1.getMargemLucro(), 0.001);
+
+        Produto p2 = new Produto();
+        p2.setPrecoCusto(30);
+        p2.setMargemLucro(1.5);
+        assertNotEquals(1.5, p2.getMargemLucro(), 0.001);
+        }
+	
+	@Test
 	void testPrecoVenda() {
 		//Ainda nao implementei o teste porque tenho que esperar outro integrante implementar margem de lucro e valor dos impostos
 	}
