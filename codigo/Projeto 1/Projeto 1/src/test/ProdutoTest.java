306e0972e4ada50f9e887fdea86672b67452ef31
friche11 <vicfriche@gmail.com>
2023-03-09 13:48:27 -0300

    Criação dos testes da classe produto com JUnit

--- codigo/Projeto 1/Projeto 1/src/test/ProdutoTest.java
+++ codigo/Projeto 1/Projeto 1/src/test/ProdutoTest.java
@@ -1,5 +1,14 @@
 package test;
 
-public class ProdutoTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProdutoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
 
 }
