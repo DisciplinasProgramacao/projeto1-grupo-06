7cadbdbbbbd9aad7e9337d82a1d283e8757fd01f
luanaf4 <103442333+luanaf4@users.noreply.github.com>
2023-03-11 15:52:35 -0300

    Atualização setMargemLucro #4

--- codigo/Projeto 1/Projeto 1/src/business/Produto.java
+++ codigo/Projeto 1/Projeto 1/src/business/Produto.java
@@ -32,7 +32,6 @@ public class Produto {
 	public void setMargemLucro(double margemLucro) {
     	if(margemLucro >= 0.3 && margemLucro <= 0.8)
     		this.margemLucro = margemLucro;
-    	setPrecoVenda(precoCusto, margemLucro, valorImpostos);
        }
     
    	public double getMargemLucro() {
