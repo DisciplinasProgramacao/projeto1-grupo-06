4540d7ab4f22dbd0002db4a76eb7fef3a58a6f13
friche11 <vicfriche@gmail.com>
2023-03-12 19:05:54 -0300

    Construtor de estoque e atributos criados

--- codigo/Projeto 1/Projeto 1/src/business/Estoque.java
+++ codigo/Projeto 1/Projeto 1/src/business/Estoque.java
@@ -2,4 +2,13 @@ package business;
 
 public class Estoque {
 
+	private static final int MAX_PRODUTOS = 1000000;
+	private Produto[] produtos;
+	private int numProdutos;
+	
+	public Estoque() {
+		produtos = new Produto[MAX_PRODUTOS];
+		numProdutos = 0;
+		
+	}
 }
