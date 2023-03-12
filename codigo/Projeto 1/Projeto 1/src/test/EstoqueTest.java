36dcb594a5118e8cf457a217258891f7d435dcb6
friche11 <vicfriche@gmail.com>
2023-03-12 19:13:40 -0300

    Teste totalEmEstoque com 0 produtos #9

--- codigo/Projeto 1/Projeto 1/src/test/EstoqueTest.java
+++ codigo/Projeto 1/Projeto 1/src/test/EstoqueTest.java
@@ -2,13 +2,34 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import business.Estoque;
 
 class EstoqueTest {
 
+Estoque estoque;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		estoque = new Estoque();
+	}
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testTotalEmEstoque() {
+		
+		assertEquals(0, estoque.totalEmEstoque(), "Testa se estoque começa vazio.");
+		
+		
+		
+	}
+	
+	void testValorTotalEmEstoque() {
+		assertEquals(0.0F, estoque.valorEmEstoque(), 0.001F, "Testa se valor em estoque começa zero.");
+		
+	
+
 	}
 
 }
