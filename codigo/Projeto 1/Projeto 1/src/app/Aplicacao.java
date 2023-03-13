4411139cd7a5dbc0bcee04cb7342eaffbbeb5212
juserrac <103602511+juserrac@users.noreply.github.com>
2023-03-13 18:58:17 -0300

    opcao 2 e 5, codigo atual para esclarecer em sala

--- codigo/Projeto 1/Projeto 1/src/app/Aplicacao.java
+++ codigo/Projeto 1/Projeto 1/src/app/Aplicacao.java
@@ -20,34 +20,50 @@ public class Aplicacao {
 	      System.out.println("6 - Sair");
 
 	      int opcao = scanner.nextInt();
-		    
-	     if (opcao == 1) {
-            System.out.println("Digite o nome do produto a ser vendido:");
-            String nome = scanner.next();
-            System.out.println("Digite a quantidade a ser vendida:");
-            int quantidade = scanner.nextInt();
-            estoque.retirarDoEstoque(nome, quantidade);
-            }
 
-          else if (opcao == 2) {
-          
-          } 
-
-          else if (opcao == 3) {
-            System.out.println("Digite o nome do produto a ser consultado:");
-            String nome = scanner.next();
-            Produto produto = estoque.buscarProduto(nome);
-            if (produto != null) {
-              System.out.println(produto.toString());
-            } else {
-              System.out.println("Produto não encontrado.");
-            }
-          } 
-		else if (opcao == 4) {
+	      if (opcao == 1) {
+	        System.out.println("Digite o nome do produto a ser vendido:");
+	        String nome = scanner.next();
+	        System.out.println("Digite a quantidade a ser vendida:");
+	        int quantidade = scanner.nextInt();
+	        estoque.retirarDoEstoque(nome, quantidade);
+	        }	         
+	      
+	      else if (opcao == 2) {
+	        System.out.println("Digite o nome do produto a ser reposto:");
+	        String nome = scanner.next();
+	        System.out.println("Digite a quantidade a ser pedida:");
+	        int quantidade = scanner.nextInt();
+	        estoque.reporEstoque(nome, quantidade);
+	        System.out.println("Pedido de reposição realizado com sucesso!");
+	      } 
+	      
+	      else if (opcao == 3) {
+	        System.out.println("Digite o nome do produto a ser consultado:");
+	        String nome = scanner.next();
+	        Produto produto = estoque.buscarProduto(nome);
+	        if (produto != null) {
+	          System.out.println(produto.toString());
+	        } else {
+	          System.out.println("Produto não encontrado.");
+	        }
+	      } 
+	      
+	      else if (opcao == 4) {
 	        System.out.println("A quantidade de produtos que constam no estoque é: " + estoque.totalEmEstoque());
 	        System.out.println("O valor total do estoque atual é: "+estoque.valorEmEstoque()); 
 	      } 
-		else if (opcao == 6) {
+	      
+	      else if (opcao == 5) {
+	        double valorEstoque = estoque.valorEmEstoque();
+	        double valorVendido = estoque.calcularValorVendido();
+	        
+	        System.out.println("Valor do estoque atual: R$" + valorEstoque);
+	        System.out.println("Valor vendido: R$" + valorVendido);
+	        System.out.println("Valor gasto em pedidos de reposição: R$" + valorGastoReposicao);
+	      } 
+	      
+	      else if (opcao == 6) {
 	        break;
 	      } 
 	      
@@ -59,5 +75,6 @@ public class Aplicacao {
 	    }
 
 	    scanner.close();
-	  }    
+	  }
+
 }
