095809e20ee0dd410fb1e68c06e3b0b2beb46941
gscantoni <104282001+gscantoni@users.noreply.github.com>
2023-03-13 18:15:05 -0300

    Criação do menu #15

--- codigo/Projeto 1/Projeto 1/src/app/Aplicacao.java
+++ codigo/Projeto 1/Projeto 1/src/app/Aplicacao.java
@@ -1,10 +1,38 @@
 package app;
+import java.util.Scanner;
+
+import business.Estoque;
+import business.Produto;
 
 public class Aplicacao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    Scanner scanner = new Scanner(System.in);
+	    Estoque estoque = new Estoque();
 
-	}
+	    while (true) {
+	      System.out.println("Selecione uma opção:");
+	      System.out.println("1 - Vender produto");
+	      System.out.println("2 - Fazer pedido de reposição de produto");
+	      System.out.println("3 - Consultar dados de um produto");
+	      System.out.println("4 - Consultar dados do estoque");
+	      System.out.println("5 - Calcular e exibir o balanço da empresa");
+	      System.out.println("6 - Sair");
 
+	      int opcao = scanner.nextInt();
+	      if (opcao == 1) {
+	      }
+		else if (opcao == 6) {
+	        break;
+	      } 
+	      
+	      else {
+	        System.out.println("Opção inválida. Tente novamente.");
+	      }
+
+	      System.out.println();
+	    }
+
+	    scanner.close();
+	  }    
 }
